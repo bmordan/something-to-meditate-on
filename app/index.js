698aa56d0dc69516c849d0ecd2d179b51794a170
Bernard Mordan <bernardmordan@gmail.com>
2017-08-22 21:45:22 +0100

    rough input layout

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native'
 
 const styles = StyleSheet.create({
@@ -10,22 +11,59 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#CEE0DC'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+  input: {
+    height: 60,
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    marginRight: 0,
+    marginLeft: 10,
+    backgroundColor: '#B9CFD4',
+    textAlign: 'left',
+    borderRadius: 2
+  },
+  button: {
+    backgroundColor: '#A5243D',
+    height: 60,
+    width: 60,
+    margin: 10,
+    padding: 10,
+    borderRadius: 2
   }
 })
 
 export default class SomethingToMeditateOn extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.onClick = (e) => {
+      console.log(this.state.value)
+    }
+  }
+
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Something to meditate on
-        </Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{fontSize: 20, textAlign: 'center', color: '#B9CFD4'}}>
+            Something to meditate on
+          </Text>
+        </View>
+        <View style={{height: 60, marginBottom: 30}}>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignSelf: 'stretch'}}>
+            <TextInput
+              value={this.state.value}
+              onChangeText={value => this.setState({value})}
+              placeholder='Enter your response here'
+              style={styles.input}
+             />
+            <View style={styles.button} onClick={this.onClick} />
+          </View>
+        </View>
       </View>
     )
   }
